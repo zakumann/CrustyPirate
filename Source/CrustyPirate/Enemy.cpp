05d9fe0b24ae3280daa72c417cf602671884741c
zakumann <samuel742able@gmail.com>
2024-11-14 23:49:29 +0900

    Create Enemy character

---
+++ Source/CrustyPirate/Enemy.cpp
@@ -0,0 +1,34 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Enemy.h"
+
+AEnemy::AEnemy()
+{
+	PrimaryActorTick.bCanEverTick = true;
+
+	PlayerDetectorSphere = CreateDefaultSubobject<USphereComponent>(TEXT("PlayerDetectorSphere"));
+	PlayerDetectorSphere->SetupAttachment(RootComponent);
+}
+
+void AEnemy::BeginPlay()
+{
+	Super::BeginPlay();
+
+	PlayerDetectorSphere->OnComponentBeginOverlap.AddDynamic(this, &AEnemy::DetectorOverlapBegin);
+	PlayerDetectorSphere->OnComponentEndOverlap.AddDynamic(this, &AEnemy::DetectorOverlapEnd);
+}
+
+void AEnemy::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+void AEnemy::DetectorOverlapBegin(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyInex, bool bFromSweep, const FHitResult& SweepResult)
+{
+
+}
+
+void AEnemy::DetectorOverlapEnd(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyInex)
+{
+
+}
---
+++ Source/CrustyPirate/Enemy.h
@@ -0,0 +1,45 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "PaperZDCharacter.h"
+
+#include "Components/SphereComponent.h"
+
+#include "PlayerCharacter.h"
+
+#include "Enemy.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class CRUSTYPIRATE_API AEnemy : public APaperZDCharacter
+{
+	GENERATED_BODY()
+
+public:
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite)
+	USphereComponent* PlayerDetectorSphere;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite)
+	APlayerCharacter* FollowTarget;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	float StopDistanceToTarget = 70.0f;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite)
+	bool IsAlive = true;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite)
+	bool CanMove = true;
+
+	AEnemy();
+	virtual void BeginPlay() override;
+	virtual void Tick(float DeltaTime) override;
+
+	UFUNCTION()
+	void DetectorOverlapBegin(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyInex, bool bFromSweep, const FHitResult& SweepResult);
+
+	UFUNCTION()
+	void DetectorOverlapEnd(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyInex);
+};
